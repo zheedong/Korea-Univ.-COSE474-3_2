17a515f7d9b8b52957193f64b51e76120bdd1249
zheedong <zheedong@gmail.com>
2023-11-25 12:55:07 +0000

    model change to lightning

--- train.py
+++ train.py
@@ -6,22 +6,23 @@ from torch.utils.data import Dataset
 tokenizer = BartTokenizer.from_pretrained('facebook/bart-base')
 model = BartForConditionalGeneration.from_pretrained('facebook/bart-base')
 
-training_args = TrainingArguments(
-    output_dir='./results',
-    num_train_epochs=3,
-    per_device_train_batch_size=16,
-    warmup_steps=500,
-    weight_decay=0.01,
-    logging_dir='./logs',
-    logging_steps=10,
-)
+import pytorch_lightning as pl
 
-trainer = Trainer(
-    model=model,
-    args=training_args,
-    train_dataset=train_dataset,
-    # You can also include a validation_dataset if you have validation data
-)
+class BartFineTuner(pl.LightningModule):
+    def __init__(self, model):
+        super().__init__()
+        self.model = model
+
+    def forward(self, input_ids, attention_mask, labels):
+        return self.model(input_ids, attention_mask=attention_mask, labels=labels)
+
+    def training_step(self, batch, batch_idx):
+        outputs = self.model(**batch)
+        loss = outputs.loss
+        return loss
+
+    def configure_optimizers(self):
+        return AdamW(self.model.parameters(), lr=5e-5)
 
 model.save_pretrained('./finetuned_bart')
 tokenizer.save_pretrained('./finetuned_bart')
@@ -30,6 +31,9 @@ def main():
     # Your code for loading data, initializing the dataset, model, tokenizer, and trainer
     # Train the model
     # Save the model and tokenizer
+    trainer = pl.Trainer(gpus=1, max_epochs=3, progress_bar_refresh_rate=30)
+    trainer.fit(BartFineTuner(model), train_loader)
+
 
 if __name__ == "__main__":
     main()
